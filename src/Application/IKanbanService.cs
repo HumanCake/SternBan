9adac2c161350f25152df477bb4b42a87be15bff
Kasper Sternberg <kaspersternberg@gmail.com>
2024-06-11 16:22:25 +0200

    Implemented TODOs in KanbanService.cs

--- src/Application/IKanbanService.cs
+++ src/Application/IKanbanService.cs
@@ -8,7 +8,7 @@ public interface IKanbanService
     Task<OperationResult<Board>> PutBoardAsync(Board board);
     Task<OperationResult<Board>> PutColumnAsync(string boardId, Column column);
     Task<OperationResult<Board>> RemoveColumnAsync(string boardId, string columnId);
-    Task<Board> PutTicket(string boardId, string columnId, Ticket ticket);
-    Task<Board> RemoveTicket(string boardId, string columnId, string ticketId);
+    Task<OperationResult<Board>> PutTicket(string boardId, string columnId, Ticket ticket);
+    Task<OperationResult<Board>> RemoveTicket(string boardId, string columnId, string ticketId);
 
 }
--- src/Application/KanbanService.cs
+++ src/Application/KanbanService.cs
@@ -6,8 +6,8 @@ namespace Application;
 
 public class KanbanService : IKanbanService
 {
-    private readonly IDatabase _database;
     private readonly IValidator<Board> _boardValidator;
+    private readonly IDatabase _database;
 
     public KanbanService(IDatabase database, IValidator<Board> boardValidator)
     {
@@ -18,10 +18,7 @@ public class KanbanService : IKanbanService
     public async Task<OperationResult<Board>> GetBoardAsync(string boardId)
     {
         var board = await _database.GetBoardAsync(boardId);
-        if (board == null)
-        {
-            return OperationResult<Board>.ErrorResult("Board not found");
-        }
+        if (board == null) return OperationResult<Board>.ErrorResult("Board not found");
 
         return OperationResult<Board>.SuccessResult(board);
     }
@@ -35,57 +32,69 @@ public class KanbanService : IKanbanService
     public async Task<OperationResult<Board>> PutColumnAsync(string boardId, Column column)
     {
         var boardResult = await GetBoardAsync(boardId);
-        if (!boardResult.Success)
-        {
-            return boardResult;
-        }
+        if (!boardResult.Success) return boardResult;
 
         var board = boardResult.Data;
         board.Columns.Add(column);
 
         var validationResult = await _boardValidator.ValidateAsync(board);
-        if (!validationResult.IsValid)
-        {
-            return OperationResult<Board>.ErrorResult(validationResult.Errors.ToString());
-        }
+        if (!validationResult.IsValid) return OperationResult<Board>.ErrorResult(validationResult.ToString());
 
-        await _database.PutBoardAsync(board);
-        return OperationResult<Board>.SuccessResult(board);
+        return await PutBoardAsync(board);
     }
 
     public async Task<OperationResult<Board>> RemoveColumnAsync(string boardId, string columnId)
     {
         var boardResult = await GetBoardAsync(boardId);
-        if (!boardResult.Success)
-        {
-            return boardResult;
-        }
+        if (!boardResult.Success) return boardResult;
 
         var board = boardResult.Data;
         var column = board.Columns.FirstOrDefault(c => c.Title == columnId);
-        if (column == null)
-        {
-            return OperationResult<Board>.ErrorResult("Column not found");
-        }
+        if (column == null) return OperationResult<Board>.ErrorResult("Column not found");
 
         board.Columns.Remove(column);
 
         var validationResult = await _boardValidator.ValidateAsync(board);
+        if (!validationResult.IsValid) return OperationResult<Board>.ErrorResult(validationResult.ToString());
+        return await PutBoardAsync(board);
+    }
+
+    public async Task<OperationResult<Board>> PutTicket(string boardId, string columnId, Ticket ticket)
+    {
+        var boardResult = await GetBoardAsync(boardId);
+        if (boardResult.Success) return boardResult;
+
+        var board = boardResult.Data;
+
+        var column = board.Columns.FirstOrDefault(c => c.Title == columnId);
+        if (column == null) return OperationResult<Board>.ErrorResult("Column not found");
+
+        column.Tickets.Add(ticket);
+        var validationResult = await _boardValidator.ValidateAsync(board);
+
         if (!validationResult.IsValid)
         {
-            return OperationResult<Board>.ErrorResult(validationResult.Errors.ToString());
+            return OperationResult<Board>.ErrorResult(validationResult.ToString());
         }
-        await _database.PutBoardAsync(board);
-        return OperationResult<Board>.SuccessResult(board);
+
+        return await PutBoardAsync(board);
     }
 
-    public Task<Board> PutTicket(string boardId, string columnId, Ticket ticket)
-    {
-        throw new NotImplementedException();
-    }
+        public async Task<OperationResult<Board>> RemoveTicket(string boardId, string columnId, string ticketId)
+        {
+            var boardResult = await GetBoardAsync(boardId);
+            if (boardResult.Success) return boardResult;
 
-    public Task<Board> RemoveTicket(string boardId, string columnId, string ticketId)
-    {
-        throw new NotImplementedException();
-    }
+            var board = boardResult.Data;
+
+            var column = board.Columns.FirstOrDefault(c => c.Title == columnId);
+            if (column == null) return OperationResult<Board>.ErrorResult("Column not found");
+            var ticket = column.Tickets.FirstOrDefault(t => t.name == ticketId);
+            if (ticket == null) return OperationResult<Board>.ErrorResult("Ticket not found");
+
+            column.Tickets.Remove(ticket);
+            var validationResult = await _boardValidator.ValidateAsync(board);
+            if (!validationResult.IsValid) return OperationResult<Board>.ErrorResult(validationResult.ToString());
+            return await PutBoardAsync(board);
+        }
 }
